e8ff7e59be174ba9a02ba6b2f2f24f3ef061adfa
fozlerabbi0 <fozlerabbi.me@gmail.com>
2024-11-01 10:02:57 +0600

    all done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,20 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 
 const Home = () => {
+    const navigation = useNavigation()
+    const location = useLocation();
+    console.log(location);
+    
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
+            {
+                navigation.state === "loading" ?
+                <p>Loading...</p> : <Outlet></Outlet>
+            }
+            
             <Footer></Footer>
         </div>
     );
